5c03680cc818018362842d1058f6a7f8ba76029c
AkiraWt <eldosatalov@gmail.com>
2019-02-21 17:49:47 +0600

    D

--- Lab01/p01.cpp
+++ Lab01/p01.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main(){
+struct Node{
+  int data;
+  Node* prev;
+  Node* next;
 
+  Node(int aData, Node* aNext, Node* aPrev)
+  : data(aData), next(aNext), prev(aPrev)
+  {
+  }
+
+
+}
+
+int main(){
+  int z;
+  while (cin >> z) {
+    new Node(z, nullpointer,);
+  }
   return 0;
 }
